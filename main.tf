f40228c9f8849eab73fe54fd880712a00cbe37bd
Andrew Graham <andrew@firehawkvfx.com>
2020-10-17 14:12:33 +1030

    alter defaults for initial deployment

--- main.tf
+++ main.tf
@@ -1,16 +1,34 @@
-provider "aws" {
-  #  if you haven't installed and configured the aws cli, you will need to provide your aws access key and secret key.
-  region = var.aws_region
-  # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
-  version = "~> 3.3.0"
-}
-
 provider "null" {
   version = "~> 3.0"
 }
 
+provider "aws" {
+  #  if you haven't installed and configured the aws cli, you will need to provide your aws access key and secret key.
+  # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
+  version = "~> 3.3.0"
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_canonical_user_id" "current" {}
+
+locals {
+  common_tags     = {
+    environment  = "prod"
+    resourcetier = "main"
+    conflictkey  = "main1" 
+    # The conflict key defines a name space where duplicate resources in different deployments sharing this name are prevented from occuring.  This is used to prevent a new deployment overwriting and existing resource unless it is destroyed first.
+    # examples might be blue, green, dev1, dev2, dev3...dev100.  This allows us to lock deployments on some resources.
+    pipelineid   = "0"
+    owner        = data.aws_canonical_user_id.current.display_name
+    accountid    = data.aws_caller_identity.current.account_id
+    terraform    = "true"
+  }
+}
+
 module "vpc" {
   source = "./modules/terraform-aws-vpc-vpn"
+  # region = data.aws_region.current.name
 
 #   firehawk_init_dependency = module.firehawk_init.init_awscli_complete
 #   create_vpc = var.enable_vpc
@@ -43,5 +61,5 @@ module "vpc" {
 #   openvpn_admin_pw   = var.openvpn_admin_pw
 
 #   vpc_name = local.name
-#   common_tags = local.common_tags
+  common_tags = local.common_tags
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+# ENV VARS
+# These secrets are defined as environment variables, or if running on an aws instance they do not need to be provided.
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+# AWS_DEFAULT_REGION
+
