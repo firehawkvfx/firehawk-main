85cd27d0ee741354a7fbdb346733877ab8864b70
Andrew Graham <andrew@firehawkvfx.com>
2020-10-25 11:59:30 +1030

    update print for py3

--- update_vars.sh
+++ update_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 to_abs_path() {
-  python -c "import os; print os.path.abspath('$1')"
+  python -c "import os; print(os.path.abspath('$1'))"
 }
 
 export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
