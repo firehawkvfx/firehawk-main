48b771a1853057fc353c62863050fd3cb9d8c66f
Andrew Graham <andrew@firehawkvfx.com>
2020-10-25 18:52:34 +1030

    install packer

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,10 @@
 sudo yum install -y jq ansible
 mkdir -p tmp
-wget https://releases.hashicorp.com/terraform/0.13.5/terraform_0.13.5_linux_amd64.zip -P tmp/
+
+wget https://releases.hashicorp.com/terraform/0.13.5/terraform_0.13.5_linux_amd64.zip -P tmp/ # Get terraform
 sudo unzip tmp/terraform_0.13.5_linux_amd64.zip -d tmp/
 sudo mv tmp/terraform /usr/local/bin/.
+
+wget https://releases.hashicorp.com/packer/1.6.4/packer_1.6.4_linux_amd64.zip -P tmp/ # Get Packer
+sudo unzip tmp/packer_1.6.4_linux_amd64.zip -d tmp/
+sudo mv tmp/packer /usr/local/bin/.
