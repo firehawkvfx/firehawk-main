495b8bd4cd8160767692ced0ab5733cf28f6c389
Andrew Graham <andrew@firehawkvfx.com>
2020-10-30 21:58:39 +1030

    update messages in shell

--- update_vars.sh
+++ update_vars.sh
@@ -35,3 +35,7 @@ if [[ ! -f $TF_VAR_inventory/hosts ]] ; then
 fi
 
 export TF_VAR_bucket_extension='firehawkvfx.com'
+
+echo "Ensure you specify your own ami id inline. eg: terraform apply -var 'ami_id=ami-12345678910'"
+echo "After deployment, ssh into the vault and init with: vault operator init -recovery-shares=1 -recovery-threshold=1"
+echo "Store the initial root token in a password manager (encrypted)."
