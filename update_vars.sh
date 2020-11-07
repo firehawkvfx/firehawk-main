5c1f879c278ea9e87e4fada2547a94ef35dd0fc1
Andrew Graham <andrew@firehawkvfx.com>
2020-11-07 21:21:35 +1030

    Update build files

--- update_vars.sh
+++ update_vars.sh
@@ -7,7 +7,6 @@ to_abs_path() {
 export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
 
 # Packer Vars
-
 export PKR_VAR_aws_region="$AWS_DEFAULT_REGION"
 if [[ -f "modules/terraform-aws-vault/examples/bastion-ami/manifest.json" ]]; then
     export PKR_VAR_bastion_centos7_ami="$(jq -r '.builds[] | select(.name == "centos7-ami") | .artifact_id' modules/terraform-aws-vault/examples/bastion-ami/manifest.json | cut -d ":" -f2)"
