6f7abc2f92474bb4bb5de6bbe21efe4a93932b63
Andrew Graham <andrew@firehawkvfx.com>
2020-11-08 14:53:14 +1030

    add timeout for nvidia install and add ts command

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # 
 
 # Build all required amis.
 set -e # Exit on error
-$SCRIPTDIR/modules/terraform-aws-vault/examples/bastion-ami/build.sh
-$SCRIPTDIR/modules/terraform-aws-vault/examples/nice-dcv-ami/build.sh
-$SCRIPTDIR/modules/terraform-aws-vault/examples/vault-consul-ami/build.sh
+$SCRIPTDIR/modules/terraform-aws-vault/examples/bastion-ami/build.sh | ts '[%H:%M:%S]'
+$SCRIPTDIR/modules/terraform-aws-vault/examples/nice-dcv-ami/build.sh | ts '[%H:%M:%S]'
+$SCRIPTDIR/modules/terraform-aws-vault/examples/vault-consul-ami/build.sh | ts '[%H:%M:%S]'
