40dee5ecb06f29db4002b162b9570a8acd94eb68
Andrew Graham <andrew@firehawkvfx.com>
2020-11-08 13:53:43 +1030

    use $SCRIPTDIR

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # The directory of this script
+
+
 # Build all required amis.
 set -e # Exit on error
-firehawk-main/modules/terraform-aws-vault/examples/bastion-ami/build.sh
-firehawk-main/modules/terraform-aws-vault/examples/nice-dcv-ami/build.sh
+$SCRIPTDIR/modules/terraform-aws-vault/examples/bastion-ami/build.sh
+$SCRIPTDIR/modules/terraform-aws-vault/examples/nice-dcv-ami/build.sh
