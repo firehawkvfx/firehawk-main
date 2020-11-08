07acedc4d92e38767381cf62b385293d39ec096d
Andrew Graham <andrew@firehawkvfx.com>
2020-11-08 14:18:23 +1030

    use abs path for manifest

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # The directory of this script
 
-
 # Build all required amis.
 set -e # Exit on error
 $SCRIPTDIR/modules/terraform-aws-vault/examples/bastion-ami/build.sh
 $SCRIPTDIR/modules/terraform-aws-vault/examples/nice-dcv-ami/build.sh
+$SCRIPTDIR/modules/terraform-aws-vault/examples/vault-consul-ami/build.sh
