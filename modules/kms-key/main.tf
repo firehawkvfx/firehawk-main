ea8a8736570f4dffe2bac75bbe44232af0b4e39a
Andrew Graham <andrew@firehawkvfx.com>
2020-11-14 11:55:59 +1030

    Add random pet string

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
   #  if you haven't installed and configured the aws cli, you will need to provide your aws access key and secret key.
   # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
-#   version = "~> 3.3.0"
+  version = "~> 3.15.0"
+}
+
+resource "random_pet" "env" {
+  length = 2
 }
 
 resource "aws_kms_key" "vault" {
@@ -21,7 +25,6 @@ resource "aws_ssm_parameter" "vault_kms_unseal" {
 
 data "aws_ssm_parameter" "vault_kms_unseal" {
   name = "vault_kms_unseal_key_id"
-  type = "SecureString"
 }
 
 data "aws_kms_key" "vault" {
