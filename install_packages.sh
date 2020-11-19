a9daf7ddc282bc5c6d56b56236c7f15a66685112
Andrew Graham <andrew@firehawkvfx.com>
2020-11-19 18:39:18 +1030

    remove moreutils

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,4 @@
-sudo yum install -y jq ansible moretutils ts
+sudo yum install -y jq ansible
 mkdir -p tmp
 
 wget https://releases.hashicorp.com/terraform/0.13.5/terraform_0.13.5_linux_amd64.zip -P tmp/ # Get terraform
