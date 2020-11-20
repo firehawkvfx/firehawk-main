34473bb45ead4691a0fc32f3c9f023049fd2fa57
Andrew Graham <andrew@firehawkvfx.com>
2020-11-20 20:28:38 +1030

    fic data ref

--- deployments/vault/main.tf
+++ deployments/vault/main.tf
@@ -75,7 +75,7 @@ data "aws_route_table" "main_private" {
 }
 
 resource "aws_route" "primary2secondary" {
-  route_table_id = aws_route_table.main_private.id
+  route_table_id = data.aws_route_table.main_private.id
   # route_table_id = data.aws_vpc.primary.main_route_table_id # ID of VPC 1 main route table.
   destination_cidr_block = data.aws_vpc.secondary.cidr_block # CIDR block / IP range for VPC 2.
   vpc_peering_connection_id = aws_vpc_peering_connection.primary2secondary.id # ID of VPC peering connection.
