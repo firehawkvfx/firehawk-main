7fb8d013f8c796db7d223a0d14bf985bf9d68138
Andrew Graham <andrew@firehawkvfx.com>
2020-11-21 14:15:58 +1030

    terraform-remote-state

---
+++ deployments/vault-configuration/vault.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  #  if you haven't installed and configured the aws cli, you will need to provide your aws access key and secret key.
+  # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
+  version = "~> 3.15.0"
+}
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_canonical_user_id" "current" {}
+
+locals {
+  common_tags     = {
+    environment  = "prod"
+    resourcetier = "main"
+    conflictkey  = "main1" 
+    # The conflict key defines a name space where duplicate resources in different deployments sharing this name are prevented from occuring.  This is used to prevent a new deployment overwriting an existing resource unless it is destroyed first.
+    # examples might be blue, green, dev1, dev2, dev3...dev100.  This allows us to lock deployments on some resources.
+    pipelineid   = "0"
+    owner        = data.aws_canonical_user_id.current.display_name
+    accountid    = data.aws_caller_identity.current.account_id
+    terraform    = "true"
+    role = "terraform remote state"
+  }
+}
+
+### Block to configure remote state
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state"
+    key            = "main/vault-configuration/terraform.tfstate"
+    region = data.aws_region.current.name
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket for terraform state"
+}
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table to lock terraform state."
+}
+### End block for remote state
+
+resource "vault_auth_backend" "example" {
+  type = "userpass"
+}
+
+resource "vault_policy" "admin_policy" {
+  name   = "admins"
+  policy = file("policies/admin_policy.hcl")
+}
+
+resource "vault_policy" "developer_policy" {
+  name   = "developers"
+  policy = file("policies/developer_policy.hcl")
+}
+
+resource "vault_policy" "operations_policy" {
+  name   = "operations"
+  policy = file("policies/operation_policy.hcl")
+}
+
+resource "vault_mount" "developers" {
+  path        = "developers"
+  type        = "kv-v2"
+  description = "KV2 Secrets Engine for Developers."
+}
+
+resource "vault_mount" "operations" {
+  path        = "operations"
+  type        = "kv-v2"
+  description = "KV2 Secrets Engine for Operations."
+}
+
+resource "vault_generic_secret" "developer_sample_data" {
+  path = "${vault_mount.developers.path}/test_account"
+
+  data_json = <<EOT
+{
+  "username": "foo",
+  "password": "bar"
+}
+EOT
+}
