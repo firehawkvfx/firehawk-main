f74a9d036a55db45b51753932fecec4427a81286
Andrew Graham <andrew@firehawkvfx.com>
2020-11-22 11:01:35 +1030

    Define source for module

--- deployments/vault-configuration/vault.tf
+++ deployments/vault-configuration/vault.tf
@@ -95,7 +95,7 @@ locals {
 # }
 
 module "update-values" {
-  path = "modules/update-values"
+  source = "modules/update-values"
   secret_tier = "dev"
   for_each = local.defaults
   secret_name = each.key
