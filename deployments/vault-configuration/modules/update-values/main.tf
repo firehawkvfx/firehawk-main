f7ed1391b15c6e67a60b33a8a1aeed5cf9dc0567
Andrew Graham <andrew@firehawkvfx.com>
2020-11-22 11:20:19 +1030

    fix syntax

--- deployments/vault-configuration/modules/update-values/main.tf
+++ deployments/vault-configuration/modules/update-values/main.tf
@@ -8,10 +8,10 @@ data "vault_generic_secret" "vault_map" { # Get the map of data at the path
 locals {
   secret_tier = var.secret_tier
   system_default = var.system_default # The system default map will define the value if value is not present, or value matches a preexisting default.
-  secret_value = { # If a present value is different to a present default, retain the vault value.  Else use the system default.
-    value = contains( keys(data.vault_generic_secret.vault_map.data), "value" ) && contains( keys(data.vault_generic_secret.vault_map.data), "default" ) && lookup( data.vault_generic_secret.vault_map.data, "value", "" ) != lookup( data.vault_generic_secret.vault_map.data, "default", "") ? lookup( data.vault_generic_secret.vault_map.data, "value", "") : local.system_default["default"] 
-  }
-  result_map = merge( local.system_default, local.secret_value )
+   # If a present value is different to a present default, retain the vault value.  Else use the system default.
+  secret_value = contains( keys(data.vault_generic_secret.vault_map.data), "value" ) && contains( keys(data.vault_generic_secret.vault_map.data), "default" ) && lookup( data.vault_generic_secret.vault_map.data, "value", "" ) != lookup( data.vault_generic_secret.vault_map.data, "default", "") ? lookup( data.vault_generic_secret.vault_map.data, "value", "") : local.system_default["default"] 
+  secret_map = { value = local.secret_value }
+  result_map = merge( local.system_default, local.secret_map )
 }
 
 resource "vault_generic_secret" "vault_map_output" {
