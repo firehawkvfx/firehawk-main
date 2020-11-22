575a2aec15e327debaf390dc7193dea9cc10180b
Andrew Graham <andrew@firehawkvfx.com>
2020-11-22 15:19:50 +1030

    Try to intialise a secret if not present

--- deployments/vault-configuration/modules/update-values/main.tf
+++ deployments/vault-configuration/modules/update-values/main.tf
@@ -1,12 +1,25 @@
 # This module will initlise vault values to a default if not present or if already matching an existing default
 # This allows us to know if a user has configured to a non default value, and if so, leave it in place.
 
+locals {
+  path = "${var.mount_path}/${var.secret_tier}/config/${var.secret_name}"
+}
+
+resource "null_resource" "init_secret" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command = <<EOT
+      vault kv put -cas=0 "${local.path}" value=""
+EOT
+  }
+}
+
 data "vault_generic_secret" "vault_map" { # Get the map of data at the path
-  path = "${var.mount_path}/${local.secret_tier}/config/${var.secret_name}"
+  depends_on = [null_resource.init_secret]
+  path = "${local.path}"
 }
 
 locals {
-  secret_tier = var.secret_tier
   system_default = var.system_default # The system default map will define the value if value is not present, or value matches a preexisting default.
    # If a present value is different to a present default, retain the vault value.  Else use the system default.
   secret_value = contains( keys(data.vault_generic_secret.vault_map.data), "value" ) && contains( keys(data.vault_generic_secret.vault_map.data), "default" ) && lookup( data.vault_generic_secret.vault_map.data, "value", "" ) != lookup( data.vault_generic_secret.vault_map.data, "default", "") ? lookup( data.vault_generic_secret.vault_map.data, "value", "") : local.system_default["default"] 
@@ -15,7 +28,7 @@ locals {
 }
 
 resource "vault_generic_secret" "vault_map_output" {
-  path = "${var.mount_path}/${local.secret_tier}/config/${var.secret_name}"
+  path = "${local.path}"
 
   data_json = jsonencode( local.result_map )
 }
