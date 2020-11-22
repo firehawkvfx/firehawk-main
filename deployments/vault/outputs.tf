523488af9712f2b47efec016e8bc590b714b1112
Andrew Graham <andrew@firehawkvfx.com>
2020-11-22 12:23:35 +1030

    escape var

--- deployments/vault/outputs.tf
+++ deployments/vault/outputs.tf
@@ -3,5 +3,5 @@ output "security_group_id_consul_cluster" {
 }
 
 output "instructions" {
-  value = "To initialise the vault, login to a server and run: vault operator init -recovery-shares=1 -recovery-threshold=1\n To connnect this node to the consul cluster, run: /opt/consul/bin/run-consul --client --cluster-tag-key \"${consul_cluster_tag_key}\" --cluster-tag-value \"${consul_cluster_tag_value}\""
+  value = "To initialise the vault, login to a server and run: vault operator init -recovery-shares=1 -recovery-threshold=1\n To connnect this node to the consul cluster, run: /opt/consul/bin/run-consul --client --cluster-tag-key \"$${consul_cluster_tag_key}\" --cluster-tag-value \"$${consul_cluster_tag_value}\""
 }
