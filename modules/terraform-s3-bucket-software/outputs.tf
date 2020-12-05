e4998d21c994bb2a2ed901cc7140a9bd1c79b038
Andrew Graham <andrew@firehawkvfx.com>
2020-12-05 13:55:11 +1030

    update output name

--- modules/terraform-s3-bucket-software/outputs.tf
+++ modules/terraform-s3-bucket-software/outputs.tf
@@ -1,3 +1,3 @@
-output "s3_bucket_software_name" {
-  value = aws_s3_bucket.shared_bucket.name
+output "s3_bucket_software_bucket_domain_name" {
+  value = aws_s3_bucket.shared_bucket.bucket_domain_name
 }
