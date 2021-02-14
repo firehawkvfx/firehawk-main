127fb6dfbb1f1f20013be2d2405b2a81347fd15d
Andrew Graham <andrew@firehawkvfx.com>
2021-02-14 12:50:11 +1030

    try revoke self

--- modules/terraform-aws-bastion/modules/bastion/user-data-auth-ssh-host-vault-token.sh
+++ modules/terraform-aws-bastion/modules/bastion/user-data-auth-ssh-host-vault-token.sh
@@ -185,7 +185,8 @@ ensure_known_hosts /home/centos/.ssh/known_hosts
 systemctl restart sshd
 
 echo "Signing SSH host key done. Revoking vault token..."
-vault token revoke $VAULT_TOKEN
+vault token revoke -self
+# vault token revoke $VAULT_TOKEN
 ### End sign SSH host key
 
 # if this script fails, we can set the instance health status but we need to capture a fault
