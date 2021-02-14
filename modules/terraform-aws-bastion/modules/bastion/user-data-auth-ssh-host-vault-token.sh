a1f60151484258c3e5c84999dfb9f60aa60d2a36
Andrew Graham <andrew@firehawkvfx.com>
2021-02-14 17:54:45 +1030

    use logging, dont disable network

--- modules/terraform-aws-bastion/modules/bastion/user-data-auth-ssh-host-vault-token.sh
+++ modules/terraform-aws-bastion/modules/bastion/user-data-auth-ssh-host-vault-token.sh
@@ -97,23 +97,22 @@ retry \
   "vault login --no-print $VAULT_TOKEN" \
   "Waiting for Vault login"
 
-echo "Aquiring vault data..."
+log "Aquiring vault data..."
 
-
-echo "Request Vault sign's the SSH host key and becomes a known host for other machines."
+log "Request Vault sign's the SSH host key and becomes a known host for other machines."
 
 # Allow access from clients signed by the CA.
 trusted_ca="/etc/ssh/trusted-user-ca-keys.pem"
 # Aquire the public CA cert to approve an authority
 vault read -field=public_key ssh-client-signer/config/ca | tee $trusted_ca
 if test ! -f "$trusted_ca"; then
-    echo "Missing $trusted_ca"
+    log "Missing $trusted_ca"
     exit 1
 fi
 
 ### Sign SSH host key
 if test ! -f "/etc/ssh/ssh_host_rsa_key.pub"; then
-    echo "Missing public host key /etc/ssh/ssh_host_rsa_key.pub"
+    log "Missing public host key /etc/ssh/ssh_host_rsa_key.pub"
     exit 1
 fi
 # Sign this host's public key
@@ -125,13 +124,13 @@ vault write -field=signed_key ssh-host-signer/sign/hostrole \
     cert_type=host \
     public_key=@/etc/ssh/ssh_host_rsa_key.pub | tee /etc/ssh/ssh_host_rsa_key-cert.pub
 if test ! -f "/etc/ssh/ssh_host_rsa_key-cert.pub"; then
-    echo "Failed to aquire /etc/ssh/ssh_host_rsa_key-cert.pub"
+    log "Failed to aquire /etc/ssh/ssh_host_rsa_key-cert.pub"
     exit 1
 fi
 chmod 0640 /etc/ssh/ssh_host_rsa_key-cert.pub
 
 # systemctl stop sshd 
-# echo "LogLevel VERBOSE" | tee --append /etc/ssh/sshd_config # for debug
+# log "LogLevel VERBOSE" | tee --append /etc/ssh/sshd_config # for debug
 
 
 
@@ -152,7 +151,7 @@ function ensure_known_hosts {
   local -r ssh_known_hosts_path="$1"
   local -r aws_external_domain=${aws_external_domain}
   if test ! -f $ssh_known_hosts_path; then
-      echo "Creating $ssh_known_hosts_path"
+      log "Creating $ssh_known_hosts_path"
       touch $ssh_known_hosts_path # ensure known hosts file exists
   fi
   if [[ "$OSTYPE" == "darwin"* ]]; then # Acquire file permissions.
@@ -161,22 +160,22 @@ function ensure_known_hosts {
       octal_permissions=$(stat --format '%a' "$ssh_known_hosts_path" | rev | sed -E 's/^([[:digit:]]{4})([^[:space:]]+)/\1/' | rev) # clip to 4 zeroes
   fi
   octal_permissions=$( python -c "print( \"$octal_permissions\".zfill(4) )" ) # pad to 4 zeroes
-  echo "$ssh_known_hosts_path octal_permissions currently $octal_permissions."
+  log "$ssh_known_hosts_path octal_permissions currently $octal_permissions."
   if [[ "$octal_permissions" != "0644" ]]; then
-      echo "...Setting to 0644"
+      log "...Setting to 0644"
       chmod 0644 $ssh_known_hosts_path
   fi
 
   grep -q "^@cert-authority \*\.consul,\*\.$aws_external_domain" $ssh_known_hosts_path || echo "@cert-authority *.consul,*.$aws_external_domain" | tee --append $ssh_known_hosts_path
   sed -i "s#@cert-authority \*\.consul,\*\.$aws_external_domain.*#@cert-authority *.consul,*.$aws_external_domain $key#g" $ssh_known_hosts_path
   ls -ltriah $ssh_known_hosts_path
-  echo "Added CA to $ssh_known_hosts_path."
+  log "Added CA to $ssh_known_hosts_path."
 }
 ensure_known_hosts /etc/ssh/ssh_known_hosts
 ensure_known_hosts /home/centos/.ssh/known_hosts
 
 systemctl stop sshd
-systemctl stop network # this can be used to avoid unknown host warnings caused by a race condition, at the expense of speed.
+# systemctl stop network # this can be used to avoid unknown host warnings caused by a race condition, at the expense of speed.
 ### Finally allow users with signed client certs to login.
 # If TrustedUserCAKeys not defined, then add it to sshd_config
 grep -q "^TrustedUserCAKeys" /etc/ssh/sshd_config || echo 'TrustedUserCAKeys' | tee --append /etc/ssh/sshd_config
@@ -184,7 +183,7 @@ grep -q "^TrustedUserCAKeys" /etc/ssh/sshd_config || echo 'TrustedUserCAKeys' | 
 sed -i "s@TrustedUserCAKeys.*@TrustedUserCAKeys $trusted_ca@g" /etc/ssh/sshd_config 
 systemctl daemon-reload
 systemctl start sshd
-systemctl start network
+# systemctl start network
 
 # centos / amazon linux, restart ssh service
 
