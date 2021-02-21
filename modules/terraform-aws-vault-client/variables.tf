84782828e0e1851e08c83053589d12126a70e6ea
Andrew Graham <andrew@firehawkvfx.com>
2021-02-21 11:38:16 +1030

    improve instructions for connection

--- modules/terraform-aws-vault-client/variables.tf
+++ modules/terraform-aws-vault-client/variables.tf
@@ -50,3 +50,8 @@ variable "aws_internal_domain" {
 #   description = "The domain used to resolve external FQDN hostnames.  Since we always provide the CA for externals connections, the default for public ec2 instances is acceptable, but in production it is best configure it with your own domain."
 #   type        = string
 # }
+
+variable "bastion_public_dns" {
+  description = "The bastion must exist in order to provide complete instructions to establish connection with this host, and also aquire the security group enabling ssh between both hosts."
+  type = string
+}
