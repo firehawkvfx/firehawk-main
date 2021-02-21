df636cd67c51782f11d472f97fefb55320cfe7f1
Andrew Graham <andrew@firehawkvfx.com>
2021-02-21 16:01:09 +1030

    update-signer-rsa-sha2-256"

--- modules/vault-configuration/main.tf
+++ modules/vault-configuration/main.tf
@@ -391,21 +391,22 @@ resource "vault_ssh_secret_backend_role" "ssh_role" {
   allow_user_certificates = true
   allowed_users           = "*"
   allowed_extensions      = "permit-pty,permit-port-forwarding"
-  default_extensions = tomap( {
-      "permit-pty" = "",
-      "permit-agent-forwarding" = "",
-      "permit-port-forwarding" = "",
-      "valid_principals": "centos,ubuntu"
-  } )
-  key_type     = "ca"
-  default_user = "centos"
+  default_extensions = tomap({
+    "permit-pty"              = "",
+    "permit-agent-forwarding" = "",
+    "permit-port-forwarding"  = "",
+    "valid_principals" : "centos,ubuntu"
+  })
+  key_type         = "ca"
+  default_user     = "centos"
+  algorithm_signer = "rsa-sha2-256"
   # valid_principals= "ubuntu,centos"
   # ttl = "30m0s"
   ttl = "720h"
   # cidr_list     = "0.0.0.0/0"
 }
 
-### SSH key signing for machines to be recognised as known hosts ###
+### SSH key signing for machines to be recognised as known hosts ### # note need to use rsa-sha2-256 now https://ibug.io/blog/2020/04/ssh-8.2-rsa-ca/
 
 resource "vault_mount" "ssh_host_signer" {
   path        = "ssh-host-signer"
@@ -426,6 +427,7 @@ resource "vault_ssh_secret_backend_role" "host_role" {
   max_ttl                 = "87600h"
   allow_host_certificates = true
   allowed_domains         = "localdomain,consul,${var.aws_external_domain}"
+  algorithm_signer        = "rsa-sha2-256"
   allow_subdomains        = true
 }
 
