bef4eddfabee9e694b780735653c7d032ddebee8
Andrew Graham <andrew@firehawkvfx.com>
2021-03-01 22:57:38 +1030

    remove some outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,13 +6,13 @@ output "public_subnets" {
   value = module.vpc.public_subnets
 }
 
-output "bastion_public_ip" {
-  value = module.vpc.bastion_public_ip
-}
+# output "bastion_public_ip" {
+#   value = module.vpc.bastion_public_ip
+# }
 
-output "bastion_graphical_public_ip" {
-  value = module.vpc.bastion_graphical_public_ip
-}
+# output "bastion_graphical_public_ip" {
+#   value = module.vpc.bastion_graphical_public_ip
+# }
 
 output "consul_client_security_group" {
   value = module.vpc.consul_client_security_group
