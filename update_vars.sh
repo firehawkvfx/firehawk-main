56d79a740f4f43e75d751c3c221d49857d37b460
Andrew Graham <andrew@firehawkvfx.com>
2021-03-01 15:52:58 +1030

    get resource tier from instance tag

--- update_vars.sh
+++ update_vars.sh
@@ -5,6 +5,18 @@ to_abs_path() {
 }
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # The directory of this script
+
+# Instance and vpc data
+export TF_VAR_deployer_ip_cidr="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)/32" # Initially there will be no remote ip onsite, so we use the cloud 9 ip.
+export TF_VAR_remote_cloud_public_ip_cidr="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)/32" # The cloud 9 IP to provision with.
+export TF_VAR_remote_cloud_private_ip_cidr="$(curl http://169.254.169.254/latest/meta-data/local-ipv4)/32"
+macid=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
+export TF_VAR_vpc_id_main_cloud9=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/${macid}/vpc-id) # Aquire the cloud 9 instance's VPC ID to peer with Main VPC
+export TF_VAR_instance_id_main_cloud9=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+
+# test aquiring the resourcetier from the instance tag.
+aws ec2 describe-tags --filters Name=resource-id,Values=$TF_VAR_instance_id_main_cloud9 --out=json|jq '.Tags[]| select(.Key == "resourcetier")|.Value'
+
 export TF_VAR_resourcetier="main" # Can be dev,green,blue,main
 export PKR_VAR_resourcetier="$TF_VAR_resourcetier"
 export TF_VAR_pipelineid="0" # Uniquely name and tag the resources produced by a CI pipeline
@@ -39,13 +51,8 @@ if [[ ! -f $public_key_path ]] ; then
     exit 1
 fi
 export TF_VAR_vault_public_key=$(cat $public_key_path)
-export TF_VAR_deployer_ip_cidr="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)/32" # Initially there will be no remote ip onsite, so we use the cloud 9 ip.
-export TF_VAR_remote_cloud_public_ip_cidr="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)/32" # The cloud 9 IP to provision with.
-export TF_VAR_remote_cloud_private_ip_cidr="$(curl http://169.254.169.254/latest/meta-data/local-ipv4)/32"
+
 export TF_VAR_log_dir="$SCRIPTDIR/tmp/log"; mkdir -p $TF_VAR_log_dir
-macid=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
-export TF_VAR_vpc_id_main_cloud9=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/${macid}/vpc-id) # Aquire the cloud 9 instance's VPC ID to peer with Main VPC
-export TF_VAR_instance_id_main_cloud9=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 export VAULT_ADDR=https://vault.service.consul:8200 # verify dns before login with: dig vault.service.consul
 export consul_cluster_tag_key="consul-servers" # These tags are used when new hosts join a consul cluster. 
