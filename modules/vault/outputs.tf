88f776b3d3b890b53a4041bcb353839784d4f103
Andrew Graham <andrew@firehawkvfx.com>
2021-03-14 17:36:49 +1030

    add private dns

--- modules/vault/outputs.tf
+++ modules/vault/outputs.tf
@@ -10,7 +10,7 @@ output "Instructions" {
   vault operator init -recovery-shares=1 -recovery-threshold=1
   vault login (Root token provided above)
 
-  Store the initial root token provided in a password manager (encrypted).
+  Store the initial root token provided in a password manager (encrypted).  Next you will configure vault policies...
 
   To connect this current instance to the vault for the first time, and updating the certificate, install vault without sudo:
   ./install-consul-vault-client --vault-module-version v0.13.11  --vault-version 1.5.5 --consul-module-version v0.8.0 --consul-version 1.8.4 --build amazonlinux2 --cert-file-path /home/ec2-user/.ssh/tls/ca.crt.pem
@@ -18,7 +18,15 @@ output "Instructions" {
   To connnect this instance you are using currently to the consul cluster on subsequent runs, and find the vault by DNS name:
   sudo /opt/consul/bin/run-consul --client --cluster-tag-key "$${consul_cluster_tag_key}" --cluster-tag-value "$${consul_cluster_tag_value}"
 
-  You should be able to login from thsi instance with:
+  You should be able to login from this instance with:
   vault login
+
+  continue to modules/vault-configuration to intialise values for first time use:
+  ./generate-plan-init
+  terraform apply "tfplan"
+
+  The apply the rest of the policies,  this step can be performed at any time you wish to update policies provided you are logged in as an admin.
+  ./generate-plan
+  terraform apply "tfplan"
 EOF
 }
