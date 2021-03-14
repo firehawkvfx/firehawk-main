dbef5ac7d7886c0970351078ff42b4b1374307ec
Andrew Graham <andrew@firehawkvfx.com>
2021-03-14 11:53:38 +1030

    update build paths

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 # Build all required amis.
 set -e # Exit on error
 
-$TF_VAR_firehawk_path/modules/terraform-aws-vault/examples/vault-consul-ami/build.sh
+$TF_VAR_firehawk_path/../packer-firehawk-amis/modules/firehawk-base-ami/build.sh
+$TF_VAR_firehawk_path/../packer-firehawk-amis/modules/firehawk-ami/build.sh
