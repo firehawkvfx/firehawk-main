5e4959b182e5f8e53d4cb061dc0bf65b8cc1f7e5
Andrew Graham <andrew@firehawkvfx.com>
2021-03-14 15:16:06 +1030

    converge update vars scripts

--- update_vars.sh
+++ update_vars.sh
@@ -49,7 +49,7 @@ export TF_VAR_remote_cloud_public_ip_cidr="$(curl http://169.254.169.254/latest/
 export TF_VAR_remote_cloud_private_ip_cidr="$(curl http://169.254.169.254/latest/meta-data/local-ipv4)/32"
 macid=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
 export TF_VAR_vpc_id_main_cloud9=$(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/${macid}/vpc-id) # Aquire the cloud 9 instance's VPC ID to peer with Main VPC
-
+export TF_VAR_cloud9_instance_name="$(aws ec2 describe-tags --filters Name=resource-id,Values=$TF_VAR_instance_id_main_cloud9 --out=json|jq '.Tags[]| select(.Key == "Name")|.Value' --raw-output)"
 export TF_VAR_account_id=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep -oP '(?<="accountId" : ")[^"]*(?=")')
 export TF_VAR_owner="$(aws s3api list-buckets --query Owner.DisplayName --output text)"
 # region specific vars
@@ -176,6 +176,8 @@ export PACKER_LOG=1
 export PACKER_LOG_PATH="packerlog.log"
 export TF_VAR_provisioner_iam_profile_name="provisioner_instance_role_$TF_VAR_conflictkey"
 export PKR_VAR_provisioner_iam_profile_name="provisioner_instance_role_$TF_VAR_conflictkey"
+export TF_VAR_packer_iam_profile_name="packer_instance_role_$TF_VAR_conflictkey"
+export PKR_VAR_packer_iam_profile_name="packer_instance_role_$TF_VAR_conflictkey"
 
 # Terraform Vars
 export TF_VAR_general_use_ssh_key="$HOME/.ssh/id_rsa" # For debugging deployment of most resources- not for production use.
@@ -197,15 +199,17 @@ export PKR_VAR_consul_cluster_tag_key="$consul_cluster_tag_key"
 export TF_VAR_consul_cluster_name="$consul_cluster_tag_value"
 export PKR_VAR_consul_cluster_tag_value="$consul_cluster_tag_value"
 
+# Retrieve SSM parameters set by cloudformation
 get_parameters=$( aws ssm get-parameters --names \
     "/firehawk/resourcetier/${TF_VAR_resourcetier}/onsite_public_ip" \
     "/firehawk/resourcetier/${TF_VAR_resourcetier}/onsite_private_subnet_cidr" \
     "/firehawk/resourcetier/${TF_VAR_resourcetier}/global_bucket_extension" \
     "/firehawk/resourcetier/${TF_VAR_resourcetier}/combined_vpcs_cidr" \
-    "/firehawk/resourcetier/${TF_VAR_resourcetier}/vpn_cidr" )
+    "/firehawk/resourcetier/${TF_VAR_resourcetier}/vpn_cidr" \
+    "/firehawk/resourcetier/${TF_VAR_resourcetier}/houdini_license_server_address" \
+    "/firehawk/resourcetier/${TF_VAR_resourcetier}/sesi_client_id" )
 
 num_invalid=$(echo $get_parameters | jq '.InvalidParameters| length')
-
 if [[ $num_invalid -eq 0 ]]; then
   export TF_VAR_onsite_public_ip=$(echo $get_parameters | jq ".Parameters[]| select(.Name == \"/firehawk/resourcetier/${TF_VAR_resourcetier}/onsite_public_ip\")|.Value" --raw-output)
   error_if_empty "SSM Parameter missing: onsite_public_ip" "$TF_VAR_onsite_public_ip"
@@ -218,15 +222,25 @@ if [[ $num_invalid -eq 0 ]]; then
   export TF_VAR_vpn_cidr=$(echo $get_parameters | jq ".Parameters[]| select(.Name == \"/firehawk/resourcetier/${TF_VAR_resourcetier}/vpn_cidr\")|.Value" --raw-output)
   error_if_empty "SSM Parameter missing: vpn_cidr" "$TF_VAR_vpn_cidr"
 
+  export TF_VAR_houdini_license_server_address=$(echo $get_parameters | jq ".Parameters[]| select(.Name == \"/firehawk/resourcetier/${TF_VAR_resourcetier}/houdini_license_server_address\")|.Value" --raw-output)
+  export PKR_VAR_houdini_license_server_address="$TF_VAR_houdini_license_server_address"
+  error_if_empty "SSM Parameter missing: houdini_license_server_address" "$TF_VAR_houdini_license_server_address"
+  export TF_VAR_sesi_client_id=$(echo $get_parameters | jq ".Parameters[]| select(.Name == \"/firehawk/resourcetier/${TF_VAR_resourcetier}/sesi_client_id\")|.Value" --raw-output)
+  export PKR_VAR_sesi_client_id="$TF_VAR_sesi_client_id"
+  error_if_empty "SSM Parameter missing: sesi_client_id" "$TF_VAR_sesi_client_id"
+
   export TF_VAR_bucket_extension="$TF_VAR_resourcetier.$TF_VAR_global_bucket_extension"
   export TF_VAR_installers_bucket="software.$TF_VAR_resourcetier.$TF_VAR_global_bucket_extension" # All installers should be kept in the same bucket.  If a main account is present, packer builds should trigger from the main account.
+  export TF_VAR_bucket_extension_vault="$TF_VAR_resourcetier.$TF_VAR_global_bucket_extension" # WARNING: if vault is deployed in a seperate tier for use, then this will probably need to become an SSM driven parameter from the template 
   # export PKR_VAR_installers_bucket="$TF_VAR_installers_bucket"
 else
   log_error "SSM parameters are not yet initialised.  You can init SSM parameters with the cloudformation template modules/cloudformation-cloud9-vault-iam/cloudformation_ssm_parameters_firehawk.yaml"
   return
 fi
 
-export TF_VAR_common_tags=$(jq -n -f "$SCRIPTDIR/common_tags.json" \
+common_tags_path="$SCRIPTDIR/common_tags.json"
+echo "read: $common_tags_path"
+export TF_VAR_common_tags=$(jq -n -f "$common_tags_path" \
   --arg environment "$TF_VAR_environment" \
   --arg resourcetier "$TF_VAR_resourcetier" \
   --arg conflictkey "$TF_VAR_conflictkey" \
