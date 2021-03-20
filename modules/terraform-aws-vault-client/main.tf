8a95f08e2799b19cb99dca953fc90af97c5b5638
Andrew Graham <andrew@firehawkvfx.com>
2021-03-20 17:37:13 +1030

    test shared vault client module

--- modules/terraform-aws-vault-client/main.tf
+++ modules/terraform-aws-vault-client/main.tf
@@ -7,27 +7,17 @@ provider "aws" {
   # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
   version = "~> 3.15.0"
 }
-
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-data "aws_canonical_user_id" "current" {}
-
-locals {
-  common_tags = var.common_tags
-}
-
-data "aws_vpc" "primary" {
+data "aws_vpc" "primary" { # this vpc
   default = false
   tags    = local.common_tags
 }
-data "aws_internet_gateway" "gw" {
-  # default = false
-  tags = local.common_tags
+data "aws_vpc" "vaultvpc" { # vault vpc
+  default = false
+  tags = local.vaultvpc_tags
 }
-
 data "aws_subnet_ids" "public" {
   vpc_id = data.aws_vpc.primary.id
-  tags   = map("area", "public")
+  tags = merge( local.common_tags, { "area" : "public" } )
 }
 
 data "aws_subnet" "public" {
@@ -37,7 +27,7 @@ data "aws_subnet" "public" {
 
 data "aws_subnet_ids" "private" {
   vpc_id = data.aws_vpc.primary.id
-  tags   = map("area", "private")
+  tags = merge( local.common_tags, { "area" : "private" } )
 }
 
 data "aws_subnet" "private" {
@@ -47,49 +37,41 @@ data "aws_subnet" "private" {
 
 data "aws_route_tables" "public" {
   vpc_id = data.aws_vpc.primary.id
-  tags   = map("area", "public")
+  tags = merge( local.common_tags, { "area" : "public" } )
 }
 
 data "aws_route_tables" "private" {
   vpc_id = data.aws_vpc.primary.id
-  tags   = map("area", "private")
+  tags = merge( local.common_tags, { "area" : "public" } )
 }
 
-data "vault_generic_secret" "private_domain" { # Get the map of data at the path
-  path = "${local.mount_path}/network/private_domain"
-}
-
-# data "vault_generic_secret" "onsite_public_ip" { # The remote onsite IP address
-#   path = "${local.mount_path}/network/onsite_public_ip"
-# }
-
-# data "vault_generic_secret" "vpn_cidr" { # Get the map of data at the path
-#   path = "${local.mount_path}/network/vpn_cidr"
-# }
-# data "vault_generic_secret" "onsite_private_subnet_cidr" { # Get the map of data at the path
-#   path = "${local.mount_path}/network/onsite_private_subnet_cidr"
-# }
 data "aws_security_group" "bastion" { # Aquire the security group ID for external bastion hosts, these will require SSH access to this internal host.  Since multiple deployments may exist, the pipelineid allows us to distinguish between unique deployments.
-  tags   = map("Name", "bastion_pipeid${lookup(local.common_tags, "pipelineid", "0")}")
-  vpc_id = data.aws_vpc.primary.id
+  tags = merge( local.common_tags, local.bastion_tags ) # Since we deploy vault alongside this account, pipelineid will probably not be an issue...  At some point we will need to create a dependency of the vault vpc output and what tags we should be using with multi account and CI.
+  vpc_id = data.aws_vpc.vaultvpc.id
 }
 
 locals {
-  mount_path           = var.resourcetier
-  vpc_id               = data.aws_vpc.primary.id
-  vpc_cidr             = data.aws_vpc.primary.cidr_block
-  aws_internet_gateway = data.aws_internet_gateway.gw.id
+  vaultvpc_tags = {
+    vpcname = var.vpcname_vault,
+    projectname = firehawk-main
+  }
+  bastion_tags = {
+    vpcname = var.vpcname_vault
+    role    = "bastion"
+    route   = "public"
+  }
+  common_tags = var.common_tags
+  mount_path  = var.resourcetier
+  vpc_id      = data.aws_vpc.primary.id
+  vpc_cidr    = data.aws_vpc.primary.cidr_block
 
   vpn_cidr                   = var.vpn_cidr
   onsite_private_subnet_cidr = var.onsite_private_subnet_cidr
 
   private_subnet_ids         = tolist(data.aws_subnet_ids.private.ids)
   private_subnet_cidr_blocks = [for s in data.aws_subnet.private : s.cidr_block]
-  private_domain             = lookup(data.vault_generic_secret.private_domain.data, "value")
   onsite_public_ip           = var.onsite_public_ip
   private_route_table_ids    = data.aws_route_tables.private.ids
-  # public_route_table_ids     = data.aws_route_tables.public.ids
-  # public_domain_name         = "none"
 }
 module "vault_client" {
   source              = "./modules/vault-client"
