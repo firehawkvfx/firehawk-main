2e199334ad0bb29e8c8f9bc2c19b26e2d4462833
Andrew Graham <andrew@firehawkvfx.com>
2021-04-02 21:29:17 +1030

    add ssh ssl and userpass modules

--- modules/vault-ssl/main.tf
+++ modules/vault-ssl/main.tf
@@ -1,20 +1,6 @@
-# You must be logged into Vault for this module to function. 
-
-provider "vault" {
-}
-
-# provider "aws" {
-#   #  if you haven't installed and configured the aws cli, you will need to provide your aws access key and secret key.
-#   # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
-#   version = "~> 3.15.0"
-# }
-
-### Auth methods ###
-resource "vault_auth_backend" "example" {
-  type = "userpass"
-}
-
 # Produce certificates for mongo
+
+provider "vault" {}
 
 resource "vault_mount" "pki" {
   path                      = "pki"
@@ -115,61 +101,3 @@ resource "vault_pki_secret_backend_role" "firehawkvfx-dot-com" {
 # >(jq -r .data.issuing_ca > issuing_ca.pem) \
 # >(jq -r .data.private_key > ca-key.pem)
 
-### SSH key signing for machines that wish to ssh to other known hosts ###
-
-resource "vault_mount" "ssh_signer" {
-  path        = "ssh-client-signer"
-  type        = "ssh"
-  description = "The SSH key signer certifying machines to authenticate ssh sessions"
-}
-
-resource "vault_ssh_secret_backend_ca" "ssh_signer_ca" {
-  backend              = vault_mount.ssh_signer.path
-  generate_signing_key = true
-}
-
-resource "vault_ssh_secret_backend_role" "ssh_role" {
-  name                    = "ssh-role"
-  backend                 = vault_mount.ssh_signer.path
-  allow_user_certificates = true
-  allowed_users           = "*"
-  allowed_extensions      = "permit-pty,permit-port-forwarding"
-  default_extensions = tomap({
-    "permit-pty"              = "",
-    "permit-agent-forwarding" = "",
-    "permit-port-forwarding"  = "",
-    "valid_principals" : "centos,ubuntu"
-  })
-  key_type         = "ca"
-  default_user     = "centos"
-  algorithm_signer = "rsa-sha2-256"
-  # valid_principals= "ubuntu,centos"
-  # ttl = "30m0s"
-  ttl = "720h"
-  # cidr_list     = "0.0.0.0/0"
-}
-
-### SSH key signing for machines to be recognised as known hosts ### # note need to use rsa-sha2-256 now https://ibug.io/blog/2020/04/ssh-8.2-rsa-ca/
-
-resource "vault_mount" "ssh_host_signer" {
-  path        = "ssh-host-signer"
-  type        = "ssh"
-  description = "The SSH host key signer enabling machines to be recognised certified known hosts"
-}
-
-resource "vault_ssh_secret_backend_ca" "ssh_host_signer_ca" {
-  backend              = vault_mount.ssh_host_signer.path
-  generate_signing_key = true
-}
-
-resource "vault_ssh_secret_backend_role" "host_role" {
-  name                    = "hostrole"
-  backend                 = vault_mount.ssh_host_signer.path
-  key_type                = "ca"
-  ttl                     = "87600h"
-  max_ttl                 = "87600h"
-  allow_host_certificates = true
-  allowed_domains         = "localdomain,consul,${var.aws_external_domain}"
-  algorithm_signer        = "rsa-sha2-256"
-  allow_subdomains        = true
-}
