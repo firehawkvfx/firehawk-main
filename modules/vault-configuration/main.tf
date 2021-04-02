9a7d6f5eb08277d5a64fd0f71b1619fe0e3cc740
Andrew Graham <andrew@firehawkvfx.com>
2021-04-02 20:29:15 +1030

    cleanup

--- modules/vault-configuration/main.tf
+++ modules/vault-configuration/main.tf
@@ -9,32 +9,26 @@ provider "aws" {
   version = "~> 3.15.0"
 }
 
-
-
 resource "vault_mount" "dev" {
   path        = "dev"
   type        = "kv-v2"
   description = "KV2 Secrets Engine for dev."
 }
-
 resource "vault_mount" "green" {
   path        = "green"
   type        = "kv-v2"
   description = "KV2 Secrets Engine for green."
 }
-
 resource "vault_mount" "blue" {
   path        = "blue"
   type        = "kv-v2"
   description = "KV2 Secrets Engine for blue."
 }
-
 resource "vault_mount" "main" {
   path        = "main"
   type        = "kv-v2"
   description = "KV2 Secrets Engine for main."
 }
-
 module "update-values-dev" { # Init defaults
   source           = "./modules/update-values"
   init             = var.init
@@ -45,7 +39,6 @@ module "update-values-dev" { # Init defaults
   system_default   = each.value
   restore_defaults = var.restore_defaults # defaults will always be updated if the present value matches a present default, but if this var is true, any present user values will be reset always.
 }
-
 module "update-values-green" { # Init defaults
   source           = "./modules/update-values"
   init             = var.init
@@ -56,7 +49,6 @@ module "update-values-green" { # Init defaults
   system_default   = each.value
   restore_defaults = var.restore_defaults # defaults will always be updated if the present value matches a present default, but if this var is true, any present user values will be reset always.
 }
-
 module "update-values-blue" { # Init defaults
   source           = "./modules/update-values"
   init             = var.init
@@ -67,7 +59,6 @@ module "update-values-blue" { # Init defaults
   system_default   = each.value
   restore_defaults = var.restore_defaults # defaults will always be updated if the present value matches a present default, but if this var is true, any present user values will be reset always.
 }
-
 module "update-values-main" { # Init defaults
   source           = "./modules/update-values"
   init             = var.init
@@ -79,83 +70,10 @@ module "update-values-main" { # Init defaults
   restore_defaults = var.restore_defaults # defaults will always be updated if the present value matches a present default, but if this var is true, any present user values will be reset always.
 }
 
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-data "aws_canonical_user_id" "current" {}
-
-
-
 ### Auth methods ###
 resource "vault_auth_backend" "example" {
   type = "userpass"
 }
-
-# resource "vault_token_auth_backend_role" "vpn_vault_token_role" {
-#   role_name              = "vpn-server-vault-token-creds-role"
-#   allowed_policies       = ["vpn_server", "ssh_host"]
-#   token_period           = 1200
-#   renewable              = true
-#   token_explicit_max_ttl = 86400
-#   # disallowed_policies = ["default"]
-#   # token_bound_cidrs = ["10.0.0.0/16"]
-#   # token_num_uses   = 1
-#   # orphan           = true
-#   # path_suffix         = "path-suffix"
-# }
-
-# resource "vault_aws_secret_backend" "aws" {
-#   # Enable dynamic generation of aws IAM user id's and secret keys
-#   path = "aws"
-#   region = data.aws_region.current.name
-#   default_lease_ttl_seconds = 600
-#   # max_lease_ttl_seconds = 60*60*24 # 1 day. Note vault must be running to revoke the credentials
-# }
-
-# module "vault_client_provisioner_iam" { # the arn of a role will turn into an id when it is created, which may change, so we probably only want to do this once, or the refs in vault will be incorrect.
-#   source = "../../modules/aws-iam-role-vault-client"
-#   role_name = "VaultUserRole"
-# }
-# resource "vault_aws_secret_backend_role" "vault_vpn_role" {
-#   backend = vault_aws_secret_backend.aws.path
-#   name    = "vpn-server-vault-iam-creds-role"
-#   credential_type = "iam_user"
-#   # role_arns             = [ module.vault_client_provisioner_iam.vault_client_role_arn ]
-
-#   policy_document = <<EOT
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "iam:*",
-#       "Resource": "*"
-#     }
-#   ]
-# }
-# EOT
-# }
-
-# resource "vault_aws_auth_backend_role" "vpn_server_aws_secret_based" {
-#   # using generated aws key based creds, allows acces to vault.  see https://discuss.hashicorp.com/t/generating-dynamic-access-keys-across-multiple-aws-accounts/5931
-#   backend                         = vault_auth_backend.aws.path
-#   token_ttl                       = 600
-#   token_max_ttl                   = 600
-#   token_policies                  = ["vpn_server"]
-#   role                            = "vpn-server-vault-iam-creds-role"
-#   auth_type                       = "iam"
-#   bound_account_ids               = [ data.aws_caller_identity.current.account_id ]
-#   bound_iam_role_arns             = [ module.vault_client_provisioner_iam.vault_client_role_arn ]
-#   # bound_iam_principal_arn = "arn:aws:iam::123456789012:role/*"
-# }
-
-# data "vault_aws_access_credentials" "creds" {
-#   backend = "aws"
-#   role    = "vpn-server-vault-iam-creds-role"
-# }
-
-# Once vault is configured below with the provisioner-vault-role, it is possible for any instance with the correct IAM profile to authenticate.
-# export VAULT_ADDR=https://vault.service.consul:8200
-# vault login -method=aws header_value=vault.service.consul role=provisioner-vault-role
 
 # Produce certificates for mongo
 
@@ -258,7 +176,6 @@ resource "vault_pki_secret_backend_role" "firehawkvfx-dot-com" {
 # >(jq -r .data.issuing_ca > issuing_ca.pem) \
 # >(jq -r .data.private_key > ca-key.pem)
 
-
 ### SSH key signing for machines that wish to ssh to other known hosts ###
 
 resource "vault_mount" "ssh_signer" {
@@ -317,16 +234,3 @@ resource "vault_ssh_secret_backend_role" "host_role" {
   algorithm_signer        = "rsa-sha2-256"
   allow_subdomains        = true
 }
-
-# resource "vault_token_auth_backend_role" "host_vault_token_role" {
-#   role_name        = "host-vault-token-creds-role"
-#   allowed_policies = ["ssh_host"]
-#   # disallowed_policies = ["default"]
-#   # token_bound_cidrs = ["10.0.0.0/16"]
-#   # token_num_uses   = 1
-#   token_period           = 6000 # 100 mins TODO reduce this after testing
-#   renewable              = true
-#   token_explicit_max_ttl = 86400
-#   # orphan           = true
-#   # path_suffix         = "path-suffix"
-# }
