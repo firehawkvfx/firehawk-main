ac1fa9235f7e46fdb22fd674d56347698b46b3cb
Andrew Graham <andrew@firehawkvfx.com>
2021-05-02 00:19:59 +0930

    ensure roles and policies exist for fleets

--- modules/terraform-aws-iam-profile-deadline-db/main.tf
+++ modules/terraform-aws-iam-profile-deadline-db/main.tf
@@ -45,6 +45,12 @@ data "aws_iam_policy_document" "assume_role" { # Determines the services able to
 #   share_with_arns = [ aws_iam_role.instance_role.arn ]
 # }
 
+module "iam_policies_s3_read_write" {
+  source = "github.com/firehawkvfx/firehawk-main.git//modules/aws-iam-policies-deadline-spot-fleet?ref=config-deadline-spot"
+  name = "DeadlineSpotFleetLauncher_${var.conflictkey}"
+  iam_role_id = aws_iam_role.instance_role.id
+}
+
 # Policy Allowing Read and write access to S3
 module "iam_policies_s3_read_write" {
   source = "github.com/firehawkvfx/firehawk-main.git//modules/aws-iam-policies-s3-read-write?ref=v0.0.3"
