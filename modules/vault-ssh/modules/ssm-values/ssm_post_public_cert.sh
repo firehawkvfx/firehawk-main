13527e81a8c8ffa3590e6dc8ccdd3c91807ee8af
Andrew Graham <queglay@gmail.com>
2021-05-15 22:18:11 +0930

    update ref

--- modules/vault-ssh/modules/ssm-values/ssm_post_public_cert.sh
+++ modules/vault-ssh/modules/ssm-values/ssm_post_public_cert.sh
@@ -6,13 +6,13 @@ echo "...Post cert to SSM parameter store."
 
 parm_name="/firehawk/resourcetier/${TF_VAR_resourcetier}/onsite_user_public_cert"
 
-cert_path="~/.ssh/remote_host/id_rsa-cert.pub"
+cert_path="/home/ec2-user/.ssh/remote_host/id_rsa-cert.pub"
 
 if [[ ! -f "$cert_path" ]]; then # Init if not set
     echo "...Failed retrieving: $cert_path}"
     exit 1
 else
-    value=$(cat ~/.ssh/remote_host/id_rsa-cert.pub)
+    value=$(cat $cert_path)
     aws ssm put-parameter \
         --name "${parm_name}" \
         --type "String" \
