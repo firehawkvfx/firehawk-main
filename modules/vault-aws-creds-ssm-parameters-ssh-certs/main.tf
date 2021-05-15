da9ae20afce3bc9352320ff1cc849a85598e8a57
Andrew Graham <queglay@gmail.com>
2021-05-16 09:08:41 +0930

    create aws creds with vault

---
+++ modules/vault-aws-creds-ssm-parameters-ssh-certs/main.tf
@@ -0,0 +1,37 @@
+# This module generates AWS credentials to read SSM parameters required to retrieve SSH certificates for a client.
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+resource "vault_aws_secret_backend" "aws" {
+}
+
+resource "vault_aws_secret_backend_role" "role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "aws-creds-ssm-parameters-ssh-certs"
+  credential_type = "iam_user"
+
+  policy_document = data.aws_iam_policy_document.read_ssm_paremeters_cert.json
+}
+
+data "aws_iam_policy_document" "read_ssm_paremeters_cert" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:DescribeParameters"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameters"
+    ]
+    resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/firehawk/resourcetier/${var.resourcetier}/*"]
+  }
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.role.name
+}
---
+++ modules/vault-aws-creds-ssm-parameters-ssh-certs/variables.tf
@@ -0,0 +1,4 @@
+variable "resourcetier" {
+  description = "The resource tier uniquely defining the deployment area.  eg: dev/green/blue/main"
+  type        = string
+}
