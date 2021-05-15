9cc0481825832e8e3b717f2a1e009ea54885b698
Andrew Graham <queglay@gmail.com>
2021-05-15 16:02:42 +0930

    dont read vault map

--- modules/vault-kv/modules/set-values/main.tf
+++ modules/vault-kv/modules/set-values/main.tf
@@ -4,11 +4,6 @@ locals {
   path = "${var.mount_path}/${var.secret_name}"
 }
 
-data "vault_generic_secret" "vault_map" { # Get the map of data at the path
-  count = 1
-  path  = local.path
-}
-
 resource "vault_generic_secret" "vault_map_output" {
   count     = 1
   path      = local.path
