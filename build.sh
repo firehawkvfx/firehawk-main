7a471251f2aa586bdb1c221432b7f5f1d8b8ad6c
Andrew Graham <queglay@gmail.com>
2021-05-16 18:13:59 +0930

    bump-versions

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build all required amis.
-set -e # Exit on error
-
-$TF_VAR_firehawk_path/../packer-firehawk-amis/modules/firehawk-base-ami/build.sh
-$TF_VAR_firehawk_path/../packer-firehawk-amis/modules/firehawk-ami/build.sh
