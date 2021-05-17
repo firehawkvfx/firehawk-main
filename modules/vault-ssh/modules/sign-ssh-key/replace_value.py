9cecdf37d1c8be71ad311ec674ece42a592be61f
Andrew Graham <queglay@gmail.com>
2021-05-17 13:55:21 +0930

    use temp file to avoid breaking sshd_config

--- modules/vault-ssh/modules/sign-ssh-key/replace_value.py
+++ modules/vault-ssh/modules/sign-ssh-key/replace_value.py
@@ -2,6 +2,10 @@ import argparse
 import sys
 import fileinput
 
+if sys.version_info[0] < 3:
+    print('You need to run this with Python 3')
+    sys.exit(1)
+
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('strings', metavar='S', type=str, nargs=2,
                     help='The key ( eg: my_key= ) and value ( eg: somevalue ) to replace any existing value with.')
@@ -11,9 +15,33 @@ parser.add_argument("-f", "--file", type=str,
 args = parser.parse_args()
 filename=args.file
 
-for line in fileinput.input([filename], inplace=True):
-    if line.strip().startswith(args.strings[0]):
-        line = '{}{}\n'.format( args.strings[0], args.strings[1] )
+starts_with = args.strings[0]
+append = args.strings[1]
+
+print( "\nFind line that starts with: {}".format( starts_with ) )
+print( "Change to: {}{}".format( starts_with, append ) )
+
+updated=False
+
+with fileinput.input(files=(filename), inplace=1) as f:
+  for line in f:
+    if line.strip().startswith(starts_with):
+        line = '{}{}\n'.format( starts_with, append )
+        updated=True
     sys.stdout.write(line)
 
-print( "Updated key/value: {}{} in {}".format( args.strings[0], args.strings[1], filename ) )
+# for line in fileinput.input([filename], inplace=True):
+#     if line.strip().startswith(starts_with):
+#         line = '{}{}\n'.format( starts_with, append )
+#         print( "\nUpdated line:" )
+#         print( line )
+#         print()
+#         updated=True
+#     else:
+#         print( "Original line: {}".format( line ) )
+#     sys.stdout.write(line)
+
+if updated:
+    print( "Updated key/value: {}{} in {}".format( starts_with, append, filename ) )
+else:
+    raise Exception( "ERROR: did not update key/value: {}{} in {}".format( starts_with, append, filename ) )
