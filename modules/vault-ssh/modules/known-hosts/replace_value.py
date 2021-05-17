710b55b5ea9aff6638ca2408b79295a78a652e3c
Andrew Graham <queglay@gmail.com>
2021-05-17 12:57:05 +0930

    ensure python 3 is used

--- modules/vault-ssh/modules/known-hosts/replace_value.py
+++ modules/vault-ssh/modules/known-hosts/replace_value.py
@@ -2,6 +2,10 @@ import argparse
 import sys
 import fileinput
 
+if sys.version_info[0] < 3:
+    print('You need to run this with Python 3')
+    sys.exit(1)
+
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('strings', metavar='S', type=str, nargs=2,
                     help='The key ( eg: my_key= ) and value ( eg: somevalue ) to replace any existing value with.')
@@ -18,16 +22,24 @@ print( "\nFind line that starts with: {}".format( starts_with ) )
 print( "Change to: {}{}".format( starts_with, append ) )
 
 updated=False
-for line in fileinput.input([filename], inplace=True):
+
+with fileinput.input(files=(filename), inplace=1) as f:
+  for line in f:
     if line.strip().startswith(starts_with):
         line = '{}{}\n'.format( starts_with, append )
-        print( "\nUpdated line:" )
-        print( line )
-        print()
         updated=True
-    else:
-        print( "Original line: {}".format( line ) )
     sys.stdout.write(line)
+
+# for line in fileinput.input([filename], inplace=True):
+#     if line.strip().startswith(starts_with):
+#         line = '{}{}\n'.format( starts_with, append )
+#         print( "\nUpdated line:" )
+#         print( line )
+#         print()
+#         updated=True
+#     else:
+#         print( "Original line: {}".format( line ) )
+#     sys.stdout.write(line)
 
 if updated:
     print( "Updated key/value: {}{} in {}".format( starts_with, append, filename ) )
