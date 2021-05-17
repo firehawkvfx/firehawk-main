53242f535721293c015f873337239312889246b3
Andrew Graham <queglay@gmail.com>
2021-05-17 12:45:23 +0930

    add verbosity

--- modules/vault-ssh/modules/known-hosts/replace_value.py
+++ modules/vault-ssh/modules/known-hosts/replace_value.py
@@ -11,16 +11,25 @@ parser.add_argument("-f", "--file", type=str,
 args = parser.parse_args()
 filename=args.file
 
+starts_with = args.strings[0]
+append = args.strings[1]
+
+print( "Find line that starts with: {}")
+print( "Change to: {}{}".format( starts_with, append ) )
+
 updated=False
 for line in fileinput.input([filename], inplace=True):
-    if line.strip().startswith(args.strings[0]):
-        line = '{}{}\n'.format( args.strings[0], args.strings[1] )
-        print( "Updated line:" )
+    if line.strip().startswith(starts_with):
+        line = '{}{}\n'.format( starts_with, append )
+        print( "\nUpdated line:" )
         print( line )
+        print()
         updated=True
+    else:
+        print( "Original line: {}".format( line ) )
     sys.stdout.write(line)
 
 if updated:
-    print( "Updated key/value: {}{} in {}".format( args.strings[0], args.strings[1], filename ) )
+    print( "Updated key/value: {}{} in {}".format( starts_with, append, filename ) )
 else:
-    raise Exception( "ERROR: did not update key/value: {}{} in {}".format( args.strings[0], args.strings[1], filename ) )
+    raise Exception( "ERROR: did not update key/value: {}{} in {}".format( starts_with, append, filename ) )
