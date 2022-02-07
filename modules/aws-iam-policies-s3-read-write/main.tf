18bb18879437577a6b0f9b76ee79a155bb43fb60
Andrew Graham <queglay@gmail.com>
2022-02-07 17:05:33 +1030

    update S3 policy

--- modules/aws-iam-policies-s3-read-write/main.tf
+++ modules/aws-iam-policies-s3-read-write/main.tf
@@ -12,25 +12,11 @@ data "aws_iam_policy_document" "s3_read_write" {
   statement {
     effect = "Allow"
     actions = [
-      "s3:GetBucketLocation",
-      "s3:ListAllMyBuckets"
-    ]
-    resources = ["*"]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:ListBucket"
-    ]
-    resources = ["arn:aws:s3:::*"]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
       "s3:PutObject",
-      "s3:GetObject",
+      "s3:Get*",
+      "s3:List*",
       "s3:DeleteObject"
     ]
-    resources = ["arn:aws:s3:::*"]
+    resources = ["*"]
   }
 }
