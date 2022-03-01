2eaddad5bb8050a93ee8ad3437a96aa923128514
Andrew Graham <queglay@gmail.com>
2022-03-01 18:14:46 +1030

    get vpc id from remote state

--- modules/terraform-aws-bastion/main.tf
+++ modules/terraform-aws-bastion/main.tf
@@ -1,72 +1,49 @@
 provider "null" {
   version = "~> 3.0"
 }
-
 provider "aws" {
   #  if you haven't installed and configured the aws cli, you will need to provide your aws access key and secret key.
   # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
   version = "~> 3.15.0"
 }
-data "aws_vpc" "primary" {
-  default = false
-  tags    = local.common_tags
+data "terraform_remote_state" "vaultvpc" {
+  backend = "s3"
+  config = {
+    bucket = "state.terraform.${var.bucket_extension_vault}"
+    key    = "firehawk-main/modules/vpc/terraform.tfstate"
+    region = data.aws_region.current.name
+  }
 }
-data "aws_internet_gateway" "gw" {
-  # default = false
-  tags = local.common_tags
+locals {
+  vpc_id = length(data.terraform_remote_state.vaultvpc.outputs.vpc_id) > 0 ? data.terraform_remote_state.vaultvpc.outputs.vpc_id : ""
+}
+data "aws_vpc" "primary" {
+  count   = length(local.vpc_id) > 0 ? 1 : 0
+  default = false
+  id      = local.vpc_id
 }
 
-data "aws_subnet_ids" "public" {
-  vpc_id = data.aws_vpc.primary.id
+data "aws_subnets" "public" {
+  count  = length(local.vpc_id) > 0 ? 1 : 0
+  vpc_id = local.vpc_id
   tags   = map("area", "public")
 }
 
 data "aws_subnet" "public" {
-  for_each = data.aws_subnet_ids.public.ids
+  count    = length(data.aws_subnets.public) > 0 ? 1 : 0
+  for_each = length(data.aws_subnets.public) > 0 ? data.aws_subnets.public[0].ids : []
   id       = each.value
-}
-
-data "aws_subnet_ids" "private" {
-  vpc_id = data.aws_vpc.primary.id
-  tags   = map("area", "private")
-}
-
-data "aws_subnet" "private" {
-  for_each = data.aws_subnet_ids.private.ids
-  id       = each.value
-}
-
-data "aws_route_tables" "public" {
-  vpc_id = data.aws_vpc.primary.id
-  tags   = map("area", "public")
-}
-
-data "aws_route_tables" "private" {
-  vpc_id = data.aws_vpc.primary.id
-  tags   = map("area", "private")
 }
 
 locals {
-  common_tags                = var.common_tags
-  mount_path                 = var.resourcetier
-  vpc_id                     = data.aws_vpc.primary.id
-  vpc_cidr                   = data.aws_vpc.primary.cidr_block
-  aws_internet_gateway       = data.aws_internet_gateway.gw.id
-  public_subnets             = tolist(data.aws_subnet_ids.public.ids)
-  public_subnet_cidr_blocks  = [for s in data.aws_subnet.public : s.cidr_block]
-  private_subnets            = tolist(data.aws_subnet_ids.private.ids)
-  private_subnet_cidr_blocks = [for s in data.aws_subnet.private : s.cidr_block]
-  onsite_public_ip           = var.onsite_public_ip
-  private_route_table_ids    = data.aws_route_tables.private.ids
-  public_route_table_ids     = data.aws_route_tables.public.ids
-  public_domain_name         = "none"
-  route_zone_id              = "none"
-  instance_name              = "${lookup(local.common_tags, "vpcname", "default")}_bastion_pipeid${lookup(local.common_tags, "pipelineid", "0")}"
+  common_tags      = var.common_tags
+  vpc_cidr         = length(data.aws_vpc.primary) > 0 ? data.aws_vpc.primary[0].cidr_block : ""
+  public_subnets   = length(data.aws_subnets.public) > 0 ? tolist(data.aws_subnets.public[0].ids) : []
+  onsite_public_ip = var.onsite_public_ip
 }
 module "bastion" {
-  source = "./modules/bastion"
-  # name                   = "bastion_pipeid${lookup(local.common_tags, "pipelineid", "0")}"
-  name                   = local.instance_name
+  source                 = "./modules/bastion"
+  name                   = "${lookup(local.common_tags, "vpcname", "default")}_bastion_pipeid${lookup(local.common_tags, "pipelineid", "0")}"
   bastion_ami_id         = var.bastion_ami_id
   consul_cluster_tag_key = var.consul_cluster_tag_key
   consul_cluster_name    = var.consul_cluster_name
@@ -78,10 +55,10 @@ module "bastion" {
   # permitted_cidr_list      = ["${local.onsite_public_ip}/32", var.remote_cloud_public_ip_cidr, var.remote_cloud_private_ip_cidr]
   public_subnet_ids        = local.public_subnets
   route_public_domain_name = var.route_public_domain_name
-  route_zone_id            = local.route_zone_id
+  route_zone_id            = "none"
   public_domain_name       = local.public_domain_name
   common_tags              = local.common_tags
   bucket_extension_vault   = var.bucket_extension_vault
   resourcetier_vault       = var.resourcetier_vault
-  vpcname_vaultvpc            = var.vpcname_vaultvpc
+  vpcname_vaultvpc         = var.vpcname_vaultvpc
 }
