f55c40e68259ac594ef20e7ff4c68fb136a01f60
Andrew Graham <queglay@gmail.com>
2022-03-09 20:39:34 +1030

    remove duplicate var

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -99,10 +99,6 @@ variable "enable_nat_gateway" {
   type        = bool
   default     = true
 }
-variable "resourcetier" {
-  description = "The resource tier uniquely defining the deployment area.  eg: dev/green/blue/main"
-  type        = string
-}
 variable "vault_vpc_subnet_count" { # If adjusting the max here, consider 2^new_bits = vault_vpc_subnet_count when constructing the subnets.
   description = "(1-4) The number of private and public subnets to use. eg: 1 will result in one public and 1 private subnet in 1AZ.  3 will result in 3 private and public subnets spread across 3 AZ's. Currently the vault cluster only uses 1 subnet."
   default     = 1
