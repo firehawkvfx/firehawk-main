848d24ef5a8e31de7d0323e1b3f3e008fffb83aa
Andrew Graham <queglay@gmail.com>
2022-03-09 19:02:23 +1030

    write ssm parameters

--- modules/vpc-data/outputs.tf
+++ modules/vpc-data/outputs.tf
@@ -2,10 +2,28 @@ output "vpc_cidr" {
   value = module.vaultvpc_all_subnet_cidrs.base_cidr_block
 }
 
+resource "aws_ssm_parameter" "vaultvpc_vpc_cidr" {
+  name  = "tmp_vaultvpc_vpc_cidr"
+  type  = "String"
+  value = module.vaultvpc_all_subnet_cidrs.base_cidr_block
+}
+
 output "public_subnets" {
   value = module.vaultvpc_all_public_subnet_cidrs.networks[*].cidr_block
 }
 
+resource "aws_ssm_parameter" "vaultvpc_vpc_cidr" {
+  name  = "tmp_vaultvpc_public_subnets"
+  type  = "String"
+  value = module.vaultvpc_all_public_subnet_cidrs.networks[*].cidr_block
+}
+
 output "private_subnets" {
+  value = module.vaultvpc_all_private_subnet_cidrs.networks[*].cidr_block
+}
+
+resource "aws_ssm_parameter" "vaultvpc_private_subnets" {
+  name  = "tmp_vaultvpc_private_subnets"
+  type  = "String"
   value = module.vaultvpc_all_private_subnet_cidrs.networks[*].cidr_block
 }
