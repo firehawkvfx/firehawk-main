4e9d6873bbe62a5e1a8af3dc3b9ba3ad246e61eb
Andrew Graham <queglay@gmail.com>
2022-04-10 11:10:43 +0930

    Remove deprecated argument

--- modules/private-tls-cert/main.tf
+++ modules/private-tls-cert/main.tf
@@ -69,8 +69,7 @@ resource "tls_cert_request" "cert" {
 
 resource "tls_locally_signed_cert" "cert" {
   cert_request_pem = tls_cert_request.cert.cert_request_pem
-
-  ca_key_algorithm   = tls_private_key.ca.algorithm
+  
   ca_private_key_pem = tls_private_key.ca.private_key_pem
   ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
 
