1b18bf49ab33b711a360edd5ebf9e6af152a8334
Andrew Graham <queglay@gmail.com>
2022-04-28 22:56:26 +0930

    Update policy to allow kms decrypt

--- modules/vault-aws-creds-ssm-parameters-ssh-certs/main.tf
+++ modules/vault-aws-creds-ssm-parameters-ssh-certs/main.tf
@@ -4,13 +4,15 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 resource "vault_aws_secret_backend_role" "role" {
-  backend = var.vault_aws_secret_backend_path # vault_aws_secret_backend.aws.path
-  name    = var.backend_name
+  backend         = var.vault_aws_secret_backend_path # vault_aws_secret_backend.aws.path
+  name            = var.backend_name
   credential_type = "iam_user"
 
   policy_document = data.aws_iam_policy_document.read_ssm_paremeters_cert.json
 }
-
+data "aws_kms_key" "ssm_key" {
+  key_id = "alias/aws/ssm"
+}
 data "aws_iam_policy_document" "read_ssm_paremeters_cert" {
   statement {
     effect = "Allow"
@@ -22,9 +24,19 @@ data "aws_iam_policy_document" "read_ssm_paremeters_cert" {
   statement {
     effect = "Allow"
     actions = [
+      "ssm:GetParametersByPath",
+      "ssm:GetParameter",
       "ssm:GetParameters"
     ]
     resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/firehawk/resourcetier/${var.resourcetier}/*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt"
+    ]
+    resources = [data.aws_kms_key.ssm_key.arn]
+    # resources = ["arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/${data.aws_kms_key.ssm_key.id}"]
   }
   statement {
     effect = "Allow"
--- modules/vault-aws-creds-ssm-parameters-ssh-certs/variables.tf
+++ modules/vault-aws-creds-ssm-parameters-ssh-certs/variables.tf
@@ -4,17 +4,17 @@ variable "resourcetier" {
 }
 variable "sqs_send_arns" {
   description = "A list of SQS queue ARNS's the secret key may send messages to"
-  type = list(string)
+  type        = list(string)
 }
 variable "sqs_recieve_arns" {
   description = "A list of SQS queue ARNS's the secret key may recieve messages from"
-  type = list(string)
+  type        = list(string)
 }
 variable "vault_aws_secret_backend_path" {
   description = "The path for the aws secret backend"
-  type = string
+  type        = string
 }
 variable "backend_name" {
   description = "The name / path where the backend will generate cred from"
-  type = string
+  type        = string
 }
